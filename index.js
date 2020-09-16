f3af91fb21d02461a8b3ab5e75b631d52b53e51c
Henrique <henrique.s3.leao@gmail.com>
2020-09-16 14:33:42 -0300

    chore: fix bug production

--- index.js
+++ index.js
@@ -15,16 +15,16 @@
 // app.listen(process.env.PORT || 3001);
 
 const fallback = require("express-history-api-fallback");
-const expressStaticGzip = require("express-static-gzip");
+// const expressStaticGzip = require("express-static-gzip");
 const express = require("express");
 const app = express();
 const root = `${__dirname}/dist`;
 
-app.use(
-  expressStaticGzip(root, {
-    enableBrotli: true,
-  })
-);
+// app.use(
+//   expressStaticGzip(root, {
+//     enableBrotli: true,
+//   })
+// );
 app.use(express.static(root));
 app.use(fallback("index.html", { root }));
 app.listen(process.env.PORT || 3001);
