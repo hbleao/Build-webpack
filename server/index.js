305e0cab04956abd34a7f9c237ef4ab3dbee8142
Henrique <henrique.s3.leao@gmail.com>
2020-09-16 16:25:14 -0300

    chore: fix bug production

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const path = require("path");
 const fallback = require("express-history-api-fallback");
 const express = require("express");
+const expressStaticGzip = require("express-static-gzip");
+
 const app = express();
 
 const root = path.resolve(__dirname, "..", "dist");
 
-app.use(express.static(root));
+app.use(
+  expressStaticGzip("dist", {
+    enableBrotli: true,
+  })
+);
 
 app.use(fallback("index.html", { root }));
-app.listen(process.env.PORT || 3001);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(
+    `Server listening on http://localhost:${PORT} in ${process.env.NODE_ENV}`
+  );
+});
