ebcd9d6a2c1a26cd879e457e209a24cee0726a9a
Henrique <henrique.s3.leao@gmail.com>
2020-09-16 15:28:14 -0300

    chore: fix bug production

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ const expressStaticGzip = require("express-static-gzip");
 const app = express();
 const root = `${__dirname}/dist`;
 
-// app.use(express.static(root));
-app.use(expressStaticGzip());
+app.use(express.static(root));
+
+app.use(
+  "/",
+  expressStaticGzip("/my/rootFolder/", {
+    enableBrotli: true,
+    customCompressions: [
+      {
+        encodingName: "deflate",
+        fileExtension: "zz",
+      },
+    ],
+    orderPreference: ["br"],
+  })
+);
 app.use(fallback("index.html", { root }));
 app.listen(process.env.PORT || 3001);
